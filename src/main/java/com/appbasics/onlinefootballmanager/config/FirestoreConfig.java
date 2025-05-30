5c52feef9b5f9d8aaea5394cfabf1b2bcced7da0
Rachid Hilmi <rachidhilmi1984@gmail.com>
2025-05-30 14:31:00 +0200

    Remove Dockerfile and switch to Railway native build

--- src/main/java/com/appbasics/onlinefootballmanager/config/FirestoreConfig.java
+++ src/main/java/com/appbasics/onlinefootballmanager/config/FirestoreConfig.java
@@ -1,5 +1,6 @@
 package com.appbasics.onlinefootballmanager.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.spring.data.firestore.repository.config.EnableReactiveFirestoreRepositories;
@@ -10,17 +11,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Base64;
 
 @Configuration
 @EnableReactiveFirestoreRepositories(basePackages = "com.appbasics.onlinefootballmanager.repositories")
 
 public class FirestoreConfig {
 
+    @Value("${FIREBASE_CONFIG_BASE64}")
+    private String firebaseConfigBase64;
+
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
+        byte[] decoded = Base64.getDecoder().decode(firebaseConfigBase64);
         GoogleCredentials credentials = GoogleCredentials.fromStream(
-                new ClassPathResource("firebase_service_account.json").getInputStream()
+                new ByteArrayInputStream(decoded)
         );
 
         FirebaseOptions options = FirebaseOptions.builder()
